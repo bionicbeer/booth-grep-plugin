e7c9d05a1cf616f835ecb65a9f44e34de58dc50b
Heart Breaker <a@example.com>
2026-07-27 20:08:04 +0800

    Init

---
+++ src/test/java/org/bionicbeer/boothgrep/BoothGrepPluginTest.java
@@ -0,0 +1,24 @@
+package org.bionicbeer.boothgrep;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import run.halo.app.plugin.PluginContext;
+
+@ExtendWith(MockitoExtension.class)
+class BoothGrepPluginTest {
+
+    @Mock
+    PluginContext context;
+
+    @InjectMocks
+    BoothGrepPlugin plugin;
+
+    @Test
+    void contextLoads() {
+        plugin.start();
+        plugin.stop();
+    }
+}
